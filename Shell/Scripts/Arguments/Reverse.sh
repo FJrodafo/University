e4871dba94a6ba2b51f4e879ebc19b5b9eb916ba
FJrodafo <cnry.fjra@gmail.com>
2023-06-30 19:17:58 +0100

    v10

---
+++ Shell/Scripts/Arguments/Reverse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Write a shell script that takes 3 arguments
+# and prints them in the reverse order.
+# Sample:
+# ./Reverse.sh Ritchie Stallman Torvalds
+echo "Original input: $1 $2 $3"
+echo "Reversed output: $3 $2 $1"
