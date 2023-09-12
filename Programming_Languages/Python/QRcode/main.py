c4bd135d8bc0da1341eae62adf2dfb49d24e4db4
FJrodafo <cnry.fjra@gmail.com>
2023-09-13 00:08:38 +0100

    v52

--- Programming_Languages/Python/QRcode/main.py
+++ Programming_Languages/Python/QRcode/main.py
@@ -3,18 +3,24 @@ import qrcode
 # URL to be encoded
 url = "https://fjrodafo.github.io"
 
-# Generate QR code
+# Create a QRCode object
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    # error_correction=qrcode.constants.ERROR_CORRECT_H,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
+
+# Add the data (in this case, the URL)
 qr.add_data(url)
 qr.make(fit=True)
 
-# Create an image from the QR Code instance
+# Create an image from the QRCode instance
 img = qr.make_image(fill="black", back_color="white")
+
+# Save the generated QR code
+img.save("qr_code.png")
 
 # Display the generated QR code
 img.show()
