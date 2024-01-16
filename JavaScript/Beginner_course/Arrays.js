9a2dbc1ad3ee6a0ad11e72d7151f4ab84bb60eac
FJrodafo <cnry.fjra@gmail.com>
2024-01-16 21:11:23 +0000

    v96

--- JavaScript/Beginner_course/Arrays.js
+++ JavaScript/Beginner_course/Arrays.js
@@ -16,9 +16,8 @@ ourArray = [50, 60, 70];
 var ourData = ourArray[0]; // equals 50
 
 myArray = [50, 60, 70];
-var myData = myArray[0];
-
-console.log(myData);
+var myData = myArray[1];
+console.log(myData); // 60
 
 // Modify array data with indexes
 ourArray = [18, 64, 99];
@@ -26,8 +25,7 @@ ourArray[1] = 45; // ourArray now equals [18, 45, 99];
 
 myArray = [18, 64, 99];
 myArray[0] = 45;
-
-console.log(myArray);
+console.log(myArray); // [45, 64, 99]
 
 // Access multi-dimensional arrays with indexes
 myArray = [
@@ -37,5 +35,42 @@ myArray = [
     [[10, 11, 12], 13, 14]
 ];
 
-myData = myArray[3][0][2]; // 12
-console.log(myData);
+myData = myArray[3][0][2];
+console.log(myData); // 12
+
+// Manipulate Arrays with push()
+ourArray = ["Stimpson", "J", "Cat"];
+ourArray.push(["happy", "joy"]); // ourArray now equals ["Stimpson", "J", "Cat", ["happy", "joy"]]
+
+myArray = [["Fran", 22], ["Cat", 2]];
+myArray.push(["Dog", 3]);
+console.log(myArray); // [["Fran", 22], ["Cat", 2], ["Dog", 3]]
+
+// Manipulate Arrays with pop()
+ourArray = [1, 2, 3];
+var removedFromOurArray = ourArray.pop(); // removedFromOurArray now equals 3, and ourArray now equals [1, 2]
+
+myArray = [["Fran", 22], ["Cat", 2]];
+var removedFromMyArray = myArray.pop();
+console.log(myArray); // [["Fran", 22]]
+
+// Manipulate Arrays with shift()
+ourArray = ["Stimpson", "J", ["Cat"]];
+var removedFromOurArray = ourArray.shift(); // removedFromOurArray now equals "Stimpson", and ourArray now equals ["J", ["Cat"]]
+
+myArray = [["Fran", 22], ["Dog", 3]];
+var removedFromMyArray = myArray.shift();
+console.log(myArray); // [["Dog", 3]]
+
+// Manipulate Arrays with unshift()
+ourArray = ["Stimpson", "J", "Cat"];
+ourArray.shift(); // ourArray now equals ["J", "Cat"]
+ourArray.unshift("Happy"); // ourArray now equals ["Happy", "J", "Cat"]
+
+myArray = [["Fran", 22], ["Dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+console.log(myArray); // [["Paul", 35], ["Dog", 3]]
+
+// Shopping list
+var myList = [["Cereals", 3], ["Milk", 2], ["Bananas", 4], ["Juice", 1], ["Eggs", 12]];
--- TODO.md
+++ TODO.md
@@ -2,8 +2,8 @@
 - [x] Java/MEGA_Curso_JAVA_desde_0_DAM_-_DAW 3.2
 - [ ] Java/MEGA_Curso_JAVA_desde_0_DAM_-_DAW 3.3
 
-- [x] JavaScript/Beginner_course 0:40:44
-- [ ] JavaScript/Beginner_course 0:46:30
+- [x] JavaScript/Beginner_course 0:46:30
+- [ ] JavaScript/Beginner_course 0:51:41
 
 - [ ] ?/db
 - [ ] ?/Mongodb
