06f50bf71f87d6c32d346442a92403204d206761
FJrodafo <cnry.fjra@gmail.com>
2024-02-25 21:39:56 +0000

    v104

--- JavaScript/Beginner_course/Functions.js
+++ JavaScript/Beginner_course/Functions.js
@@ -67,3 +67,60 @@ function myOutfit() {
 
 console.log(myOutfit());
 console.log(outerWear);
+
+// Return a value from a function with return
+function minusSeven(num) {
+    return num - 7;
+}
+
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(minusSeven(10)); // 3
+console.log(timesFive(5)); // 25
+
+// Understanding undefined value returned from a function
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum += 5;
+}
+
+addThree(); // Undefined
+addFive(); // Undefined
+
+// Assignment with a returned value
+var changed = 0;
+var processed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+changed = change(10);
+processed = processArg(7);
+
+console.log(changed); // 5
+console.log(processed); // 2
+
+// Stand in line
+function nextInLine(arr, item) {
+    arr.push(item);
+    // return item;
+    return arr.shift();
+}
+
+var testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr)); // Before: [1,2,3,4,5]
+console.log(nextInLine(testArr, 6)); // 1
+console.log("After: " + JSON.stringify(testArr)); // After: [2,3,4,5,6]
--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 - [x] Java/MEGA_Curso_JAVA_desde_0_DAM_-_DAW/*.java 3.3
 - [ ] Java/MEGA_Curso_JAVA_desde_0_DAM_-_DAW/*.java 3.4
 
-- [x] JavaScript/Beginner_course/*.js 0:51:41
-- [ ] JavaScript/Beginner_course/*.js 1:02:40
+- [x] JavaScript/Beginner_course/*.js 1:02:40
+- [ ] JavaScript/Beginner_course/*.js 1:08:41
 
 - [x] SQL/*.sql 1:41:13
 - [ ] SQL/*.sql 1:59:53
