4488127019501a8856548f5313b468f0c2db95a2
FJrodafo <cnry.fjra@gmail.com>
2024-04-02 21:14:08 +0100

    v159

--- CFGS/DAW/BAE/MySQL_Docker/launch_mysql.sh
+++ CFGS/DAW/BAE/MySQL_Docker/launch_mysql.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /home/user/path/to/MySQL_Docker
-docker pull mysql
-docker pull phpmyadmin/phpmyadmin
 docker-compose up -d
--- CFGS/DAW/BAE/T1_Supermercado/supermercado.sql
+++ CFGS/DAW/BAE/T1_Supermercado/supermercado.sql
@@ -4,27 +4,30 @@ USE supermercado;
 
 -- Tabla de Clientes
 CREATE TABLE Clientes (
-    dni varchar(9) NOT NULL UNIQUE PRIMARY KEY,
+    dni varchar(9) NOT NULL UNIQUE,
     nombre varchar(25) NOT NULL,
     apellidos varchar(25) NOT NULL,
     direccion varchar(100) NOT NULL,
-    fecha_nacimiento DATE NOT NULL CHECK (fecha_nacimiento < CURRENT_DATE)
+    fecha_nacimiento DATE NOT NULL CHECK (fecha_nacimiento < CURRENT_DATE),
+    PRIMARY KEY (dni)
 );
 
 -- Tabla de Proveedores
 CREATE TABLE Proveedores (
-    nif varchar(9) NOT NULL UNIQUE PRIMARY KEY,
+    nif varchar(9) NOT NULL UNIQUE,
     nombre varchar(25) NOT NULL,
-    direccion varchar(100) NOT NULL
+    direccion varchar(100) NOT NULL,
+    PRIMARY KEY (nif)
 );
 
 -- Tabla de Productos
 CREATE TABLE Productos (
-    codigo int NOT NULL UNIQUE PRIMARY KEY,
+    codigo int NOT NULL UNIQUE,
     nombre varchar(25) NOT NULL,
     precio decimal(6,2) NOT NULL CHECK (precio > 0),
     nif_proveedor varchar(9) NOT NULL,
-    FOREIGN KEY(nif_proveedor) REFERENCES Proveedores(nif)
+    PRIMARY KEY (codigo),
+    CONSTRAINT fk_productos_proveedores FOREIGN KEY (nif_proveedor) REFERENCES Proveedores(nif)
 );
 
 -- Tabla de Relación Clientes_Productos
@@ -32,7 +35,7 @@ CREATE TABLE Clientes_Productos (
     -- id int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
     dni_cliente varchar(9) NOT NULL,
     codigo_producto int NOT NULL,
-    FOREIGN KEY(dni_cliente) REFERENCES Clientes(dni),
-    FOREIGN KEY(codigo_producto) REFERENCES Productos(codigo),
-    PRIMARY KEY(dni_cliente, codigo_producto)
+    PRIMARY KEY (dni_cliente, codigo_producto),
+    CONSTRAINT fk_clientes_clientes_productos FOREIGN KEY (dni_cliente) REFERENCES Clientes(dni),
+    CONSTRAINT fk_clientes_productos_productos FOREIGN KEY (codigo_producto) REFERENCES Productos(codigo)
 );
