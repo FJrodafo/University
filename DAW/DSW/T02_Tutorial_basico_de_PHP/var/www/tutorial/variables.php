f743c059bfe497c1d241db77ab387a46177ab09b
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-10-06 20:36:03 +0100

    Upload DAW/DSW/T02 Tutorial básico de PHP

---
+++ DAW/DSW/T02_Tutorial_basico_de_PHP/var/www/tutorial/variables.php
@@ -0,0 +1,14 @@
+<?php
+    $author = "John Doe";
+    echo "<h1>Hello, World!</h1>
+    <p>This dynamic web page was created by $author.</p>";
+
+    $author = "Max Mustermann";
+    echo "<p>Supported by $author.</p>";
+
+    echo '$author';
+
+    $author1 = "John Doe";
+    $author2 = "Max Mustermann";
+    echo "<p>This dynamic web page was created by $author1 and $author2.</p>";
+?>
