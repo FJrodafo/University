3fe3aefa0bfe1dd33314165b06ac1b4ef31776b3
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-10-14 14:43:44 +0100

    Fix DAW/DSW/Master Ejercicios

--- DAW/DSW/Master/var/www/daw/ejercicios/calcula_la_suma/resultado.php
+++ DAW/DSW/Master/var/www/daw/ejercicios/calcula_la_suma/resultado.php
@@ -11,18 +11,18 @@
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Calcula la suma</title>
-</head>
-<body>
-    <h2>Resultado</h2>
-    <?php if ($esCorrecto): ?>
-        <p><?= $num1 ?> + <?= $num2 ?> = <?= $resultadoSuma ?> ✅ ¡Correcto!</p>
-    <?php else: ?>
-        <p><?= $num1 ?> + <?= $num2 ?> = <?= $respuesta ?> ❌ Incorrecto...</p>
-    <?php endif; ?>
-    <button type="button" onclick="window.location.href='formulario.php'">Volver al formulario</button>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Calcula la suma</title>
+    </head>
+    <body>
+        <h2>Resultado</h2>
+        <?php if ($esCorrecto): ?>
+            <p><?= $num1 ?> + <?= $num2 ?> = <?= $resultadoSuma ?> ✅ ¡Correcto!</p>
+        <?php else: ?>
+            <p><?= $num1 ?> + <?= $num2 ?> = <?= $respuesta ?> ❌ Incorrecto...</p>
+        <?php endif; ?>
+        <button type="button" onclick="window.location.href='formulario.php'">Volver al formulario</button>
+    </body>
 </html>
