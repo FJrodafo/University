b56496aac9c6779c7722c09f3539693992782f13
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-10-18 14:07:32 +0100

    Fix via_get.php DAW/DSW/Master

--- DAW/DSW/Master/var/www/tutorial/examples/data_transfer/via_get.php
+++ DAW/DSW/Master/var/www/tutorial/examples/data_transfer/via_get.php
@@ -1,5 +1,5 @@
 <?php
-    // http://tutorial.fjrodafo.com/data_transfer/via_get.php?forename=Fran&surname=Afonso
+    // http://tutorial.fjrodafo.com/examples/data_transfer/via_get.php?forename=Fran&surname=Afonso
     $var1 = $_GET['forename'];
     $var2 = $_GET['surname'];
     echo "Hello " . $var1 . " " . $var2 . "!";
